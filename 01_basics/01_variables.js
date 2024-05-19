cf7c9acb743539ce10762f0c2cf3f206bd89a21c
chetan barola <98829361+ChetanBarola@users.noreply.github.com>
2024-05-19 11:22:30 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144;
+
+// accountId = 21; // not allowed 
+
+let accountName = "chetan";
+let accountPassword = "123456";
+
+accountName = "barola";
+accountPassword = "56789";
+
+console.table([accountId, accountName, accountPassword]);
+
